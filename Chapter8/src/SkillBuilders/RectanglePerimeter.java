7e5a699534b8719467eab658fe257e0442b6d318
511026006 <511026006@L815WD220021.edu.cbe.ab.ca>
2026-09-14 11:09:58 -0600

    11:09am

---
+++ Chapter8/src/SkillBuilders/RectanglePerimeter.java
@@ -0,0 +1,12 @@
+package SkillBuilders;
+
+public class RectanglePerimeter {
+    public static void main(String[] args) {
+        int width = 4;
+        int length = 13;
+
+        int perimeter = 2 * width + 2 * length;
+
+        System.out.println("The perimeter of the rectangle is " + perimeter);
+    }
+}
