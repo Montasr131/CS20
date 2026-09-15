a86f0f3b909c2bf3b080918ab7b112bad11584dd
511026006 <511026006@L815WD220021.edu.cbe.ab.ca>
2026-09-15 09:37:37 -0600

    9:37am

---
+++ Chapter3/src/SkillBuilders/Distance.java
@@ -0,0 +1,13 @@
+package SkillBuilders;
+public class Distance {
+    public static void main(String[] args) {
+        
+        double firstSegment = 12.2;
+        double secondSegment = 10.6;
+        double thirdSegment = 5.8;
+        
+        double totalDistance = firstSegment + secondSegment + thirdSegment;
+        
+        System.out.println("The total distance is " + totalDistance + " km.");
+    }
+}
